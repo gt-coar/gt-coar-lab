a0b1c975fa4166a698e8bb2a59e84ceabb5c3083
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2020-05-26 14:12:39 -0400

    start pipelining

---
+++ scripts/build.py
@@ -0,0 +1,44 @@
+import subprocess
+import jinja2
+import pathlib
+import ruamel_yaml
+import sys
+from constructor.conda_interface import cc_platform
+import datetime
+
+HERE = pathlib.Path(__file__).parent
+ROOT = HERE.parent
+INSTALLER = ROOT / "installer"
+TMPL = INSTALLER / "construct.yaml.j2"
+CONSTRUCT = installer / "construct.yaml"
+PROJ = ROOT / "anaconda-project.yml"
+LOCK = ROOT / "anaconda-project-lock.yml"
+ENV = "coar-lab"
+
+
+def template():
+    """ Build a construct.yaml from the project file, lock file
+    """
+    lock = ruamel_yaml.safe_load(LOCK.read_text())
+    proj = ruamel_yaml.safe_load(PROJ.read_text())
+    env = lock
+    tmpl = jinja2.Template(TMPL.read_text())
+    today = datetime.today()
+    packages = lock["env_specs"][ENV]["packages"]
+    context = dict(
+        version=f"{today.year}.{today.month}.{today.day}",
+        channels=proj[ENV]["channels"],
+        specs=sorted([*packages.get("all", []), *packages.get(cc_platform, [])])
+    )
+    CONSTRUCT.write_text(tmpl.render(**context))
+    return 0
+
+
+def build():
+    """ Build an installer from the generated construct.yaml
+    """
+    return subprocess.call(["constructor", "."], cwd=str(INSTALLER))
+
+
+if __name__ == "__main__"
+    sys.exit(template() or build())
