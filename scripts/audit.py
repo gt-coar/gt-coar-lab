5d60d5db4cff4a912f44777ff35b3fd3ec50d556
Nicholas Bollweg <nicholas.bollweg@gtri.gatech.edu>
2020-06-25 14:02:38 -0400

    #2: add safety ignores

--- scripts/audit.py
+++ scripts/audit.py
@@ -4,22 +4,53 @@
 """ standalone script for running safety
 """
 
-import pkgutil
+import os
 import subprocess
 import sys
+import tarfile
 from pathlib import Path
 
+import requests
+
 HERE = Path(__file__)
-ATEST_OUT = HERE.parent.parent / "atest" / "output"
+ROOT = HERE.parent.parent
+ATEST_OUT = ROOT / "atest" / "output"
+IGNORE_IDS = os.environ.get("SAFETY_IGNORE_IDS", "").strip().split()
+SAFEY_DB_URL = os.environ["SAFETY_DB_URL"]
+SAFETY_PATH = ROOT / ".cache" / "safety-db"
+SAFETY_TARBALL = SAFETY_PATH / "safety-db.tar.gz"
+
+
+def fetch_db():
+    if not SAFETY_TARBALL.exists():
+        SAFETY_PATH.mkdir(parents=True, exist_ok=True)
+        r = requests.get(SAFEY_DB_URL)
+        SAFETY_TARBALL.write_bytes(r.content)
+        tf = tarfile.open(SAFETY_TARBALL)
+        tf.extractall(SAFETY_PATH)
 
 
 def safety(req_files):
     req_args = sum([["-r", req] for req in ATEST_OUT.rglob("requirements.txt")], [])
     assert req_args, "no requirements.txt found to audit"
-    loader = pkgutil.get_loader("safety_db")
-    db = Path(loader.path).parent
-    args = ["safety", "check", "--db", db, "--full-report", *req_args]
-    return subprocess.check_call(list(map(str, args)))
+    fetch_db()
+    ignores = sum([["--ignore", id_] for id_ in IGNORE_IDS], [])
+    args = list(
+        map(
+            str,
+            [
+                "safety",
+                "check",
+                "--db",
+                next(SAFETY_PATH.glob("*/data")),
+                "--full-report",
+                *ignores,
+                *req_args,
+            ],
+        )
+    )
+    print("safety args\n", " ".join(args))
+    return subprocess.check_call(args)
 
 
 if __name__ == "__main__":
